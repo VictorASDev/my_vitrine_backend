597feb1e0d4b93b700f3f66048acc172e7f04fec
Victor <victor.augustosilva004@gmail.com>
2026-09-02 22:43:47 -0300

    fix: corrige userService

--- src/main/java/com/myvitrine/api/service/UserService.java
+++ src/main/java/com/myvitrine/api/service/UserService.java
@@ -31,7 +31,7 @@ public class UserService {
             throw new ResourceConflictException("Ja existe um usuario cadastrado com o e-mail " + request.email());
         }
         User user = new User(UUID.randomUUID(), request.name(), request.email(),
-                passwordEncoder.encode(request.password()), request.profileType(), LocalDateTime.now());
+                passwordEncoder.encode(request.password()), null, LocalDateTime.now());
         return UserResponse.from(userRepository.save(user));
     }
 
@@ -52,7 +52,6 @@ public class UserService {
         user.setName(request.name());
         user.setEmail(request.email());
         user.setPasswordHash(passwordEncoder.encode(request.password()));
-        user.setProfileType(request.profileType());
         return UserResponse.from(user);
     }
 
