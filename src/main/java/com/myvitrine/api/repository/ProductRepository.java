085ae53fbdd08e13bc6e3943944accfd4e717f47
Victor <victor.augustosilva004@gmail.com>
2026-09-04 23:16:22 -0300

    feat: corrige lazy initialization

--- src/main/java/com/myvitrine/api/repository/ProductRepository.java
+++ src/main/java/com/myvitrine/api/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package com.myvitrine.api.repository;
 import com.myvitrine.api.model.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,9 +13,17 @@ import java.util.UUID;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
+    @EntityGraph(attributePaths = {"store"})
     List<Product> findByStoreUserId(UUID storeId);
 
+    @EntityGraph(attributePaths = {"store"})
     Page<Product> findByStoreUserId(UUID storeId, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"store"})
     List<Product> findByActiveTrue();
+
+    @EntityGraph(attributePaths = {"store"})
+    @Override
+    java.util.Optional<Product> findById(UUID id);
 }
+
